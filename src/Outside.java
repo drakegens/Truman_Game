e8ac7f658673f43e3290c98fc72e72331db8e6ba
Knarral <kerf2648@gmail.com>
2014-12-02 20:57:18 -0600

    s

--- src/Outside.java
+++ src/Outside.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  * @author Seth Martin
@@ -19,6 +21,41 @@ public class Outside extends Room{
 	
 	public void encounter() {
 		Zombie zombie = new Zombie("zomb");
-		
+		Scanner in = new Scanner(System.in);
+		while (zombie.getTurns() >= 0 || zombie.getHealth() > 0) {
+			System.out.println("The zombie has " + zombie.getTurns() + "more turns before it reaches you. \n"
+					+ "The zombie has " + zombie.getHealth() + "health left. \n"
+							+ "You can: \n"
+							+ "Shoot the zombie. \n"
+							+ "Try to run. \n"
+							+ "Change weapons. \n "
+							+ "What do you do?");
+			String response = in.nextLine();
+			if (response == "Shoot the zombie.") {
+				
+			}
+			else if (response == "Try to run.") {
+				if (weather == 2) {
+					if (20 * (int) Math.random() + 1 >= 18) { //roll a d20, if a 18 or higher is rolled, success
+						zombie.changeHealth(0);
+					}
+					else {
+						zombie.changeTurns(zombie.getTurns() - 1);
+						System.out.println("You failed to run away.");
+					}
+				}
+				else {
+					if (20 * (int) Math.random() + 1 >= 15) { //roll a d20, if a 15 or higher is rolled, success
+						
+					}
+				}
+			}
+			else if (response == "Change weapons.") {
+				
+			}
+			else {
+				System.out.println("Invalid response.");
+			}
+		}
 	}
 }
